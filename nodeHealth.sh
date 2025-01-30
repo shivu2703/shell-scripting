d7dbe76c455e31aeb0a2d944a9ae54b8608652e6
Shivangi <shga2797@gmail.com>
2025-01-30 11:35:34 +0000

    delated day-2

---
+++ nodeHealth.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+##################################
+#  Author: Shivangi
+#  Date: 30-01-2025
+#
+# This script tell the node health
+# Version: v1
+#
+# ###############################
+
+set -x
+
+df -h
+
+free -m
+
+nproc
